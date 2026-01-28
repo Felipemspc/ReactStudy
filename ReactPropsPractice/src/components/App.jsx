9e1931ef421009a1b6c5a04cb387058e0944bfd7
Felipe Matos Costa <mspcfelipe@gmail.com>
2026-01-28 12:33:17 -0300

    React props training again

--- ReactPropsPractice/src/components/App.jsx
+++ ReactPropsPractice/src/components/App.jsx
@@ -1,34 +1,32 @@
 import React from "react";
-import Cards from "./Cards";
+import Card from "./Card";
 import contacts from "../contacts";
-
 function App() {
-  return (
-console.log(contacts),
-
-    <div>
-      <h1 className="heading">My Contacts</h1>  
-      <Cards 
-      name = {contacts[0].name}
-      imgURL = {contacts[0].imgURL} 
-      cellphone = {contacts[0].phone}
-      email = {contacts[0].email} 
-      />
-      <Cards 
-      name = {contacts[1].name}
-      imgURL = {contacts[1].imgURL} 
-      cellphone = {contacts[1].phone}
-      email = {contacts[1].email} 
-      />
-      <Cards 
-      name = {contacts[2].name}
-      imgURL = {contacts[2].imgURL} 
-      cellphone = {contacts[2].phone}
-      email = {contacts[2].email} 
-      />
-     
-    </div>
   
+  return ( 
+    <div className="container">
+      <h1 className="heading">My Contacts</h1>
+      <div className="cardContainer">
+      <Card
+      name = {contacts[0].name}
+      img = {contacts[0].imgURL}
+      phone = {contacts[0].phone}
+      email = {contacts[0].email}
+      />
+      <Card
+      name = {contacts[1].name}
+      img = {contacts[1].imgURL}
+      phone = {contacts[1].phone}
+      email = {contacts[1].email}
+      />
+      <Card
+      name = {contacts[2].name}
+      img = {contacts[2].imgURL}
+      phone = {contacts[2].phone}
+      email = {contacts[2].email}
+      />
+      </div>
+    </div>
   );
 }
 
