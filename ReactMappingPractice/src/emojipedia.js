fcd0b5200a9091bea70bf05ea2ba13822ca2b269
Felipe Matos Costa <mspcfelipe@gmail.com>
2026-02-20 20:15:27 -0300

    React Map() Exercise

---
+++ ReactMappingPractice/src/emojipedia.js
@@ -0,0 +1,55 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😀",
+    name: "Grinning Face",
+    meaning:
+      "A smiley face with a big grin. Often used to express happiness, excitement, or friendliness."
+  },
+  {
+    id: 5,
+    emoji: "😂",
+    name: "Face with Tears of Joy",
+    meaning:
+      "epresents laughter, something extremely funny."
+  },
+  {
+    id: 6,
+    emoji: "😉",
+    name: "Winking Face",
+    meaning:
+      "A face with a wink. Often used to convey a secret, playful, or flirtatious message."
+  },
+  {
+    id: 7,
+    emoji: "😎",
+    name: "Smiling Face with Sunglasses",
+    meaning:
+      "A smiling face wearing sunglasses. Often used to express coolness, confidence, or to show that something is awesome."
+  }
+  
+];
+
+
+export default emojipedia;
