1b4434910afff050c2256c323dc8e01262790714
Felipe Matos Costa <mspcfelipe@gmail.com>
2026-03-10 17:56:00 -0300

    Minor modifications on file.

--- ReactConditionalRendering/src/components/App.jsx
+++ ReactConditionalRendering/src/components/App.jsx
@@ -4,14 +4,14 @@ import Login from "./Login"
 
 let isLoggedIn = false;
 
-let currentTime = new Date(2026, 11 , 2, 11).getHours()
+let currentTime = new Date(2026, 11 , 2, 16).getHours()
 console.log(currentTime)
 
 function App() {
   return (
     <div className="container">{
         //TERNARY
-      isLoggedIn ? <h1>Hello MuthaFUckas</h1> : <Login /> 
+       isLoggedIn ? <h1>You are logged in.</h1> : <Login /> 
         //&& OPERATOR
        //currentTime > 12 && <h1>Why Are you Still Working!?</h1>
     
