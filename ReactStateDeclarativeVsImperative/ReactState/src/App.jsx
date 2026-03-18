9173d58ae7bcfaa0c580d27099d9a97f5b9fe5e5
Felipe Matos Costa <mspcfelipe@gmail.com>
2026-03-18 16:10:00 -0300

    Minor changes on App.css

--- ReactStateDeclarativeVsImperative/ReactState/src/App.jsx
+++ ReactStateDeclarativeVsImperative/ReactState/src/App.jsx
@@ -2,25 +2,33 @@ import React from 'react'
 import './App.css'
 
 
+function toggleStrike(){
+  const texto = document.getElementById('text');
+  if(texto.style.textDecoration === 'line-through'){
+    texto.style.textDecoration = null
+  }else{
+    texto.style.textDecoration='line-through'
+  }
+}
 
-function strike (){
-    
-    document.getElementById("texto").style.textDecoration = "line-through"
 
+
+/*function strikeThrough (){
+  return document.getElementById("text").style.textDecoration = "line-through"
 }
 
 function unstrike(){
-  document.getElementById('texto').style.textDecoration = null;
-}
+  return document.getElementById('text').style.textDecoration = null}
 
-
+*/
 function App() {
 
   return <div>
-     <h1 id='texto'>Hello</h1>
-  <button id='botao' onClick={strike}>Press to change</button>
-  <button id='botao' onClick={unstrike}>Press to change</button>
- </div>
+    <h1 id='text'>Hi There, </h1>
+    <p>MuthaFuckas!</p>
+    <button onClick={toggleStrike}>Strike / Unstrike Through</button>
+
+   </div>
 }
 
 export default App
