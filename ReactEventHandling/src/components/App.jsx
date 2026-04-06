d1007d11bc5821b67cd4c5b38846c36604911f93
Felipe Matos Costa <mspcfelipe@gmail.com>
2026-04-06 16:21:42 -0300

    React Event Handling Exercise

--- ReactEventHandling/src/components/App.jsx
+++ ReactEventHandling/src/components/App.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React , {useState} from "react";
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function App() {
+
+const [headingText, setHeadingText] = useState("Click To submitt!")
+const [isMouseOver , setMouseOver] = useState(false)
+function changeHeadingText(){
+  console.log("Clicked")
+  setHeadingText("Submitted!")
+}
+
+function handleMouseOver(){
+  console.log("O mouse está sobre o botão!")
+  setMouseOver(true)
+}
+
+function handleMouseOut(){
+  console.log("O mouse saiu do botão!")
+  setMouseOver(false)
+}
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button onClick={changeHeadingText} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} style={{backgroundColor: isMouseOver ? "black" : "pink"}}>Submit</button>
     </div>
   );
 }
