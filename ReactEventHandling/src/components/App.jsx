3a77aa565600a68ede1e410e92477efbe9a5dfb3
Felipe Matos Costa <mspcfelipe@gmail.com>
2026-06-03 14:47:18 -0300

    Exercicio de React how to handle events Refeito.

--- ReactEventHandling/src/components/App.jsx
+++ ReactEventHandling/src/components/App.jsx
@@ -1,49 +1,32 @@
-import React, { useState } from "react";
+import React , {useState} from "react";
+
+
+
 
 function App() {
-  const [headingText, setHeadingText] = useState("Hello");
-  const [isMouseover, setMouseOver] = useState(false);
-  const [isMouseDown, setMouseDown] = useState(false);
-  function handleOnClick() {
-    console.log("Clicando em submit!");
-    setHeadingText("Submitted");
-  }
-  function handleMouseOver() {
-    console.log("Cursor em cima do botao submit");
-    setMouseOver(true);
-  }
-  function handleMouseOut() {
-    console.log("Mouse saindo da botão submit");
-    setMouseOver(false);
-  }
-  function handleMouseDown(){
-    console.log("Mouse  foi clicado")
-    setMouseDown(true)
 
-  }
-  function handleMouseUp(){
-    setMouseDown(false)
-  }
+const [headingText, setHeadingText] = useState("Hello")
+const [isMouseOver, setMouseOver] = useState(false)
+function handleMouseOver(){
+  
+  setMouseOver(true)
+}
 
+function handleMouseLeave(){
+  setMouseOver(false)
+}
+function handleClick(){
+  setHeadingText("Submitted!")
+  
+}
+
+ 
+  
   return (
     <div className="container">
       <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button
-        onClick={handleOnClick}
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-        onMouseDown={handleMouseDown}
-        onMouseUp={handleMouseUp}
-        
-        style={{
-          backgroundColor: isMouseover ? "black" : "white",
-          color: isMouseover ? "white" : "black",
-          boxShadow: isMouseDown ? "10px 10px 10px red" : "none",transition:"box-shadow 0.2 ease-in-out",
-        }}
-      >
-        Submit
-      </button>
+      <button onClick={handleClick} onMouseOver={handleMouseOver} style={{backgroundColor: isMouseOver? "black" : "white"}} onMouseLeave={handleMouseLeave}>Submit</button>
     </div>
   );
 }
--- ReactEventHandling/src/index.jsx
+++ ReactEventHandling/src/index.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 import App from "./components/App";
 
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-
-root.render(<App />);
+root.render(<App/>)
 
 // If you're running this locally in VS Code use the commands:
 // npm install
