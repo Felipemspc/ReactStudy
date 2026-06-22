6de56f5ce736aa300a0cb229e83d0dc678142aa8
Felipe Matos Costa <mspcfelipe@gmail.com>
2026-06-22 10:02:39 -0300

    React Form

--- ReactForms/src/components/App.jsx
+++ ReactForms/src/components/App.jsx
@@ -4,30 +4,26 @@ import React, {useState} from "react";
 
 
 
-function App() {
-  
-const [name, setName] = useState("")
-const [nomeSalvo,setNomeSalvo] = useState("")
 
-function handleChange(event){
-  console.log(event.target.value)
-  setName(event.target.value)
- 
-}  
-function handleClick(event){
-  setNomeSalvo(name)
-  event.preventDefault();
+function App() {
+
+const [guest,setGuest] = useState("")
+const [headingText, setText] = useState("")
+
+function handleChange(){
+  
+  setGuest(event.target.value)
 }
 
+function handleClick(){
+  setText(guest) 
+}
 
-  
   return (
     <div className="container">
-      <h1>Hello {nomeSalvo} </h1>
-      <form>
-      <input onChange={handleChange} type="text" placeholder="What's your name?" value={name} />
-      <button onClick={handleClick} >Submit</button>
-      </form>
+      <h1>Hello {headingText} </h1>
+      <input type="text" placeholder="What's your name?" onChange={handleChange} value={guest}/>
+      <button onClick={handleClick}>Submit</button> 
     </div>
   );
 }
